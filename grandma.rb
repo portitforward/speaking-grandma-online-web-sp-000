1f0d687d724d7e190e8b6d0d45e5d76c1a7808db
Cody Fussell <cody.fussell@gmail.com>
2019-09-09 23:44:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,9 +17,9 @@ def speak_to_grandma(phrase)
     if phrase != phrase.upcase
       return "HUH?! SPEAK UP, SONNY!"
     elsif phrase == "I LOVE YOU GRANDMA!"
-      puts "I LOVE YOU TOO PUMPKIN!"
+      return "I LOVE YOU TOO PUMPKIN!"
     else
-      puts "NO, NOT SINCE 1938!"
+      return "NO, NOT SINCE 1938!"
     end 
 end  
   
